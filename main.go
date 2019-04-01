9399edc5b1989c843f2611a95b1b77c2069325ae
Anton Marin <antonaryo85@gmail.com>
2019-04-01 07:41:04 +0300

    cobra init

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/manifoldco/promptui"
+	"github.com/antonmarin/skeleton/cmd"
 )
 
 func main() {
-	prompt := promptui.Select{
-		Label: "Select",
-		Items: []string{"Docker", "Vagrant"},
-	}
-	_, result, err := prompt.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	fmt.Printf("You choose %q\n", result)
+	cmd.Execute()
 }
