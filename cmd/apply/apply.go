1c027198678696febb24158a882e0c4a7d95a86b
Anton Marin <antonaryo85@gmail.com>
2019-04-01 08:13:15 +0300

    typo

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -10,7 +10,7 @@ var Cmd = &cobra.Command{
 	Use:   "apply",
 	Short: "Apply skeleton to project",
 	Long: `Apply skeleton to project files.
-Be careful it would replace previous skeleton blocks.`,
+Be careful, it would replace previous skeleton blocks.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		prompt := promptui.Select{
 			Label: "Select",
