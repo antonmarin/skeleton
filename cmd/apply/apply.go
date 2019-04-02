c688221410d9b3e0d04457c4057143a3dbfd0986
Anton Marin <antonaryo85@gmail.com>
2019-04-02 07:42:49 +0300

    apply useCase

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -1,8 +1,6 @@
 package apply
 
 import (
-	"fmt"
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -12,17 +10,11 @@ var Cmd = &cobra.Command{
 	Long: `Apply skeleton to project files.
 Be careful, it would replace previous skeleton blocks.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		prompt := promptui.Select{
-			Label: "Select",
-			Items: []string{"Docker", "Vagrant"},
-		}
-		_, result, err := prompt.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return err
-		}
 
-		fmt.Printf("You choose %q\n", result)
 		return nil
 	},
+}
+
+type applyUseCase interface {
+	Execute() error
 }
--- useCases/apply/apply.go
+++ useCases/apply/apply.go
@@ -1,1 +1,34 @@
 package apply
+
+type apply struct {
+	configFactory  configFactory
+	configObserver configObserver
+}
+
+func NewApply(configFactory configFactory, configObserver configObserver) *apply {
+	return &apply{configFactory: configFactory, configObserver: configObserver}
+}
+
+func (useCase apply) Execute() error {
+	config, err := useCase.configFactory.ConstructConfig()
+	if err != nil {
+		return err
+	}
+
+	if err = useCase.configObserver.Update(config); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type configFactory interface {
+	ConstructConfig() (Config, error)
+}
+
+type Config interface {
+}
+
+type configObserver interface {
+	Update(Config) error
+}
